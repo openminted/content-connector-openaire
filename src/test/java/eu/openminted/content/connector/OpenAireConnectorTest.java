22dd7eeee701918cb46f6ea292a3410510f6f0b6
kgiann78 <kgiann78@gmail.com>
2016-12-19 21:03:18 +0200

    code cleanup: cleaning unnecessary comments

--- src/test/java/eu/openminted/content/connector/OpenAireConnectorTest.java
+++ src/test/java/eu/openminted/content/connector/OpenAireConnectorTest.java
@@ -17,15 +17,12 @@ public class OpenAireConnectorTest {
         // To alter the query by a parameter or field or facet
         // feel free to comment or add anything
 
-
         Query query = new Query();
         query.setFrom(0);
         query.setTo(10);
         query.setParams(new HashMap<>());
-//        query.getParams().put("fl", new ArrayList<>());
-//        query.getParams().get("fl").add("__result");
-        query.getParams().put("fq", new ArrayList<>());
-        query.getParams().get("fq").add("__indexrecordidentifier:*00680ab21c76269e780f5e9e7e636619");
+//        query.getParams().put("fq", new ArrayList<>());
+//        query.getParams().get("fq").add("__indexrecordidentifier:*00680ab21c76269e780f5e9e7e636619");
         query.getParams().put("sort", new ArrayList<>());
         query.getParams().get("sort").add("__indexrecordidentifier asc");
         query.setKeyword("*:*");
@@ -68,8 +65,6 @@ public class OpenAireConnectorTest {
         OpenAireConnector openAireConnector = new OpenAireConnector();
         Query query = new Query();
         query.setParams(new HashMap<>());
-//        query.getParams().put("fl", new ArrayList<>());
-//        query.getParams().get("fl").add("__result");
 
         query.getParams().put("fq", new ArrayList<>());
         query.getParams().get("fq").add("__indexrecordidentifier:*00680ab21c76269e780f5e9e7e636619");
